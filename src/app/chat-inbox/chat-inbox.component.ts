b9eb01c47a3526cbaf49d9fa768afb5955613fdb
srirang <srirang@leena.ai>
2023-05-28 13:06:56 +0530

    type(feat): send message and read from socket mvp finished

--- src/app/chat-inbox/chat-inbox.component.ts
+++ src/app/chat-inbox/chat-inbox.component.ts
@@ -16,14 +16,35 @@ export class ChatInboxComponent implements OnInit {
   }
   
   setupSocketConnection() {
-    console.log('setting socket connection')
-     this.socket = io(SOCKET_ENDPOINT);
-  }
+    this.socket = io(SOCKET_ENDPOINT);
+    this.socket.on('message-broadcast', (data: string) => {
+    if (data) {
+     const element = document.createElement('li');
+     element.innerHTML = data;
+     element.style.background = 'white';
+     element.style.padding =  '15px 30px';
+     element.style.margin = '10px';
+
+     //@ts-ignore //TODO:
+     document.getElementById('message-list').appendChild(element);
+     }
+   });
+ }
 
 
   sendMessage() {
     this.socket.emit('message', this.message);
+    const element = document.createElement('li');
+    element.innerHTML = this.message;
+    element.style.background = 'white';
+    element.style.padding =  '15px 30px';
+    element.style.margin = '10px';
+    element.style.textAlign = 'right';
+console.log(document.getElementById('message-list'))
+    //@ts-ignore
+    document.getElementById('message-list').appendChild(element);
+    
     this.message = '';
-  }
+ }
 }
 
